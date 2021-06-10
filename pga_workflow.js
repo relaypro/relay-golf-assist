d6b059e62ac2a08f9303a58df53d76c1468d7562
shakeebshams <shakeebshams@gmail.com>
2021-06-10 11:39:27 -0400

    complete state maneegement for relay workflow

--- pga_workflow.js
+++ pga_workflow.js
@@ -5,25 +5,23 @@ const { Event, Taps, Button, createWorkflow, notificationEvent } = pkg
 
 const createApp = (relay) => {
     console.log("app is hosted and running")
+    let text
+    let session_id
+    let state
 
     relay.on(Event.START, async () => {
-        //let deviceName = await relay.getDeviceName()
-        //let deviceId = await relay.getDeviceId()
-        //let text = await relay.getVar(`text`)
-        //let channel = await relay.getVar(`channel`)
-        //let ts = await relay.getVar(`ts`)
-        //let response_url = await relay.getVar(`response_url`)
         relay.alert(`pga`,`trigger recieved.`,['990007560159094'])
     })
 
     relay.on(`start`, async () => {
-        let text = await relay.getVar(`text`)
-        let session_id = await relay.getVar(`session_id`)
+        text = await relay.getVar(`text`)
+        session_id = await relay.getVar(`session_id`)
+        state = 0
         console.log("session ID from within workflow: " + session_id)
         console.log(text)
-        await relay.say(text)
-        await relay.say("tap once to accept")
-        await relay.say("double tap to reject")
+        await relay.say(`Pickup requested at ${text} 
+            tap once to accept, double tap to reject
+        `)
     })
 
     relay.on(`button`, async (button, taps) => {
@@ -33,24 +31,40 @@ const createApp = (relay) => {
         if (button.button === `action`) {
             console.log("action button")
             if (button.taps === `single`) {
-                await relay.say("pickup request accepted")
-                let state = 1
-                await axios.post(`https://relay-pga.herokuapp.com/request/stage/${state}/${session_id}`,
-                    {
-                        name: "shams",
-                        cart_number: "14"
-                    }
-                )
-                await relay.terminate()
+                if (state === 0) {
+                    await relay.say("pickup request accepted")
+                    state = 1
+                    await axios.post(`https://relay-pga.herokuapp.com/request/stage/${state}/${session_id}`,
+                        {
+                            name: "shams",
+                            cart_number: "14"
+                        }
+                    )
+                } else if (state === 1) {
+                    await relay.say("drop off request completed")
+                    state = 2
+                    await axios.post(`https://relay-pga.herokuapp.com/request/stage/${state}/${session_id}`,
+                        {
+                            name: "shams",
+                            cart_number: "14"
+                        }
+                    )
+                    await relay.terminate()
+                }
+
             } else if (button.taps === `double`) { 
-                await relay.say(`Request terminated`)
-                let terminating_id = await relay.getDeviceId()
-                await axios.post(`https://relay-pga.herokuapp.com/request/reject/${session_id}`,
-                    {
-                        device_id: terminating_id,
-                    }
-                )
-                await relay.terminate()
+                if (state === 0) {
+                    await relay.say(`Request terminated`)
+                    let terminating_id = await relay.getDeviceId()
+                    await axios.post(`https://relay-pga.herokuapp.com/request/reject/${session_id}`,
+                        {
+                            device_id: terminating_id,
+                        }
+                    )
+                    await relay.terminate()
+                } else {
+                    await relay.say(`Pickup requested at ${text}`)
+                }
             }
         }
     })
